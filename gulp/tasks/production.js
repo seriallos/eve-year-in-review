4a4a11a77277f84d500ef0d471e4f2dbe74ca395
Dave Hendler <sollaires@gmail.com>
2015-02-28 11:24:30 -0800

    Copy CSS in production task

--- gulp/tasks/production.js
+++ gulp/tasks/production.js
@@ -3,5 +3,5 @@ var gulp = require('gulp');
 // Run this to compress all the things!
 gulp.task('production', function(){
   // This runs only if the karma tests pass
-  gulp.start(['markup', 'images', 'iconFont', 'minifyCss', 'uglifyJs'])
+  gulp.start(['copyfiles', 'markup', 'images', 'iconFont', 'minifyCss', 'uglifyJs'])
 });
